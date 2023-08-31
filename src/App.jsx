fb6c136c3c62f8daac81e8439580c8a6b0e3bfef
Moktadir Rahman <autocircled@gmail.com>
2023-08-31 17:02:13 +0600

    working hard

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import { Todos, FavColor } from './components/Todos.jsx'
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState(["todo 1", "todo 2", "todo 3"]);
-
-  const increment = () => {
-    setCount(c => c + 1);
-  }
+const App = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-
-      <Todos todos={todos} />
-      <FavColor />
-
-      <hr />
-      <div>
-        <p>Count: {count}</p>
-        <button onClick={increment}>Increment</button>
-      </div>
-    </>
+    <div className="App">
+      <h1>My Playground</h1>
+    </div>
   )
 }
 
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,17 +1,29 @@
-import React, { useState, memo } from "react";
+import React, { useState, memo, useEffect } from "react";
 import { ReactDOM } from "react";
 
 export const FavColor = () => {
+    const [date, setDate] = useState(new Date());
     const [textStyle, setTextStyle] = useState({
         color: "red",
         fontSize: "20px",
         fontWeight: 100,
         fontStyle: "italic"
     });
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setDate(new Date());
+    //     }, 1000);
+    //     console.log("Counting:", date.toLocaleTimeString());
+
+    // }, [date]);
     return (
         <>
             <h1 style={textStyle}>My Fav Color is <span>{textStyle.color}</span></h1>
-            <button onClick={() => setTextStyle(previousState => { return { ...previousState, color: "blue" } })}>Blue</button>
+            {/* <h2>{date.toLocaleTimeString()}</h2> */}
+            <button onClick={() => setTextStyle(previousState => {
+                return { ...previousState, color: "blue" }
+            })}>Blue</button>
             <button onClick={() => setTextStyle(previousState => { return { ...previousState, color: "red" } })}>Red</button>
             <button onClick={() => setTextStyle(previousState => { return { ...previousState, fontSize: "50px" } })}>Font Increase</button>
             <button onClick={() => setTextStyle(previousState => { return { ...previousState, fontSize: "20px" } })}>Font Decrease</button>
